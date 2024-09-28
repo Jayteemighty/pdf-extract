fb41a492db3ad505d8a65e1a991cec49caf1aefd
Adesina Joshua <tolujosh1@gmail.com>
2024-09-28 17:12:38 +0100

    changes for deployment

--- backend/pdf/views.py
+++ backend/pdf/views.py
@@ -11,6 +11,8 @@ from .serializers import PDFDataSerializer
 
 # Download necessary NLTK resources
 nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger_eng')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('stopwords')
 
